65a7389a845a7ae63251ecd607ed9d48a13b8e66
jserna18 <jserna18@unm.edu>
2016-11-17 12:13:48 -0700

    Initial commit

---
+++ src/KnuthMorrisPratt.java
@@ -0,0 +1,148 @@
+/**
+
+ ** Java Program to implement Knuth Morris Pratt Algorithm
+
+ **/
+
+
+
+import java.io.BufferedReader;
+
+import java.io.File;
+import java.io.InputStreamReader;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+
+/** Class KnuthMorrisPratt **/
+
+public class KnuthMorrisPratt
+
+{
+
+  /** Failure array **/
+
+  private int[] failure;
+
+  /** Constructor **/
+
+  public KnuthMorrisPratt(String string, String pattern)
+
+  {
+
+    /** pre construct failure array for a pattern **/
+
+    int arraySize = pattern.length();
+
+    failure = new int[arraySize];
+
+    fail(pattern, arraySize);
+
+    /** find match **/
+
+    int pos = posMatch(string, pattern);
+
+    if (pos == -1)
+
+      System.out.println("\nNo match found");
+
+    else
+
+      System.out.println("\nMatch found at index "+ pos);
+
+  }
+
+  /** Failure function for a pattern **/
+
+  private void fail(String pattern, int arraySize)
+
+  {
+
+
+    failure[0] = -1;
+
+    for (int j = 1; j < arraySize; j++)
+
+    {
+
+      int i = failure[j - 1];
+
+      while ((pattern.charAt(j) != pattern.charAt(i + 1)) && i >= 0)
+
+        i = failure[i];
+
+      if (pattern.charAt(j) == pattern.charAt(i + 1))
+
+        failure[j] = i + 1;
+
+      else
+
+        failure[j] = -1;
+
+    }
+
+  }
+
+  /** Function to find match for a pattern **/
+
+  private int posMatch(String string, String pattern)
+
+  {
+
+    int i = 0, j = 0;
+
+    int stringLength = string.length();
+
+    int patternLength = pattern.length();
+
+    while (i < stringLength && j < patternLength)
+
+    {
+
+      if (string.charAt(i) == pattern.charAt(j))
+
+      {
+
+        i++;
+
+        j++;
+
+      }
+
+      else if (j == 0)
+
+        i++;
+
+      else
+
+        j = failure[j - 1] + 1;
+
+    }
+
+    return ((j == patternLength) ? (i - patternLength) : -1);
+
+  }
+
+  /** Main Function **/
+
+  public static void main(String[] args) throws IOException
+
+  {
+
+//    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    Scanner scanner = new Scanner(new File(args[0]));
+
+    String string = scanner.nextLine();
+    System.out.println(string);
+
+    scanner = new Scanner(new File(args[1]));
+    String pattern = scanner.nextLine();
+    System.out.println(pattern);
+
+    KnuthMorrisPratt kmp = new KnuthMorrisPratt(string, pattern);
+
+  }
+
+}
---
+++ src/StringGenerator.java
@@ -0,0 +1,104 @@
+import java.io.*;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Created by jserna18 on 11/17/16.
+ */
+public class StringGenerator
+{
+  BufferedWriter writer;
+  File logFile;
+  Random rand;
+
+  private void generateString()
+  {
+    rand = new Random();
+
+
+    try {
+      //create a temporary file
+      logFile = new File("Small");
+
+      // This will output the full path where the file will be written to...
+      System.out.println(logFile.getCanonicalPath());
+
+      writer = new BufferedWriter(new FileWriter(logFile));
+
+      for (int i = 0; i < 100; i++)
+      {
+
+        int ascii = rand.nextInt(122 - 97 + 1) + 97;
+
+        try
+        {
+          writer.write((char)ascii);
+        }
+        catch (IOException e)
+        {
+          e.printStackTrace();
+        }
+
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        // Close the writer regardless of what happens...
+        writer.close();
+      } catch (Exception e) {
+
+      }
+    }
+  }
+
+  private void generatePattern()
+  {
+    try
+    {
+      Scanner scan = new Scanner(logFile);
+      String string = scan.nextLine();
+      int maxLocation = string.length() - 10;
+      int maxPatternLength = 10;
+
+      int patternLocation = rand.nextInt(maxLocation - 1 + 1) + 1;
+      String pattern = string.substring(patternLocation, patternLocation+maxPatternLength);
+
+      logFile = new File("SmallPattern");
+      try
+      {
+        writer = new BufferedWriter(new FileWriter(logFile));
+
+        writer.write(pattern);
+      }
+      catch (IOException e)
+      {
+        e.printStackTrace();
+      }
+
+    }
+    catch (FileNotFoundException e)
+    {
+      e.printStackTrace();
+    }
+    finally {
+      try {
+        // Close the writer regardless of what happens...
+        writer.close();
+      } catch (Exception e) {
+
+      }
+    }
+
+  }
+
+
+  public static void main(String[] args)
+  {
+   StringGenerator str = new StringGenerator();
+
+    str.generateString();
+    str.generatePattern();
+
+  }
+}
